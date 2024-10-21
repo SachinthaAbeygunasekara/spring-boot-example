13721ae0d6b4b0ac1e8c9548d5f73aa31c7fd8ad
SachinthaAbeygunasekara <112684802+SachinthaAbeygunasekara@users.noreply.github.com>
2024-10-21 18:48:05 +0530

    Revert "Read Customer API (GET) is created."
    This reverts commit 5fabbb7a40ee4ed709e2618f7f5c700c06f99390.

--- src/main/java/com/sample/Main.java
+++ src/main/java/com/sample/Main.java
@@ -2,27 +2,9 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("api/v1/customers")
 public class Main {
-
-    private final CustmorRepository custmorRepository;
-
-    public Main(CustmorRepository custmorRepository) {
-        this.custmorRepository = custmorRepository;
-    }
-
-    @GetMapping
-    public List<Customer> getCustomers(){
-        return custmorRepository.findAll();
-    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
